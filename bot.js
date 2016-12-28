5a91c25b957aefb99b3f7d3a0bd2af63922eec85
Klosterman <itsandyklost@gmail.com>
2016-12-28 17:10:24 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ function respond() {
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
-    postMessage(rules);
+    postMessage("heck");
     this.res.end();
   } else {
     console.log("don't care");
